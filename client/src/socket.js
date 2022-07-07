3233afffcecb38f3ae963c843ea2f66985c148f0
eric zhang <ezhang323@gmail.com>
2022-07-07 13:00:09 -0700

    https?

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 const io = require("socket.io-client");
-const socket = io("http://coalition-chess.herokuapp.com")
+const socket = io("https://coalition-chess.herokuapp.com")
 //const socket = io("localhost:2354")
 export default socket
